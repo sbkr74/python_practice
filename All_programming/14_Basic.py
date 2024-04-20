9e6760db7932a3dcce5de0db0fd0f25462bf234b
Shubham Biruly <45933577+sbkr74@users.noreply.github.com>
2024-04-20 14:01:05 +0530

    14_Basic.py file updated2.0

--- All_programming/14_Basic.py
+++ All_programming/14_Basic.py
@@ -1,1 +1,10 @@
-
+# Largest among three digit
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+c = int(input("Enter third number:"))
+if(a>b) and (a>c):
+  print("first number %d is the largest" %a)
+elif b>c:
+  print("second number %d is the largest" %b)
+else:
+  print("third number %d is the largest" %c)
