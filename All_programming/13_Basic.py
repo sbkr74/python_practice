1b7f02e6cab3b17240dee0b176d5da611c088402
Shubham Biruly <45933577+sbkr74@users.noreply.github.com>
2024-04-20 09:19:57 +0530

    13_Basic.py file updated2.0

--- All_programming/13_Basic.py
+++ All_programming/13_Basic.py
@@ -1,1 +1,9 @@
 # leap year program
+year = int(input("Enter a year: "))
+if year%400==0:
+  print("%d is leap year." %year)
+else:
+  if year%100==0:
+    print("%d is not leap year." %year)
+  elif year%4==0:
+    print("% is leap year." %year)
