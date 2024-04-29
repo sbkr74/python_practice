7701d4528c2c0d86a31af4947b156085f06bb3b8
sbkr74 <sbkr74@gmail.com>
2024-04-29 23:36:09 +0530

    25_Basic.py file added

---
+++ All_programming/25_Basic.py
@@ -0,0 +1,6 @@
+# Convert decimals to binary, octal, hexaDecimal
+num = int(input("Enter a nmber:"))
+print("Deciaml:",num)
+print("Binary:",bin(num))
+print("Octal:",oct(num))
+print("hexaDecimal:",hex(num))
