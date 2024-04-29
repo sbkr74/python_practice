29c4c10dbdf8e3e415c8efcd003fe1a469ae214d
sbkr74 <sbkr74@gmail.com>
2024-04-29 23:28:28 +0530

    24_Basic.py file added

---
+++ All_programming/24_Basic.py
@@ -0,0 +1,22 @@
+# Find numbers divisible by another number.
+# using anonymous function
+'''
+there will be list having some random numbers.
+I WILL TAKE INPUT FROM USER UPTO WHICH THEY WANT TO CHECK i.e another number
+then we will store and print it
+'''
+
+'''
+Example:
+[12,13,39,30,55,65]
+2 = 12,30
+3 = 12,39,30
+4 = 12
+5 = 30,55,65
+6 = 12,30
+7 = none
+7  = none 
+.
+.
+.
+'''
