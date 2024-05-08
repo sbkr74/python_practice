bf7e200cecab21563e074a9433d7d2632fe06860
sbkr74 <sbkr74@gmail.com>
2024-05-08 21:19:12 +0530

    30_Basic.py file updated2.0

--- All_programming/30_Basic.py
+++ All_programming/30_Basic.py
@@ -4,3 +4,20 @@
 # sub(a,b) -> diff(a-b)
 # mult(a,b) -> prod(a*b)
 # div(a,b) -> div(a//b)
+def add(a,b):
+    add = a+b
+    return add
+def sub(a,b):
+    sub = a-b
+    return sub
+def mult(a,b):
+    mult = a*b
+    return mult
+def div(a,b):
+    div = a//b
+    return div
+
+if __name__ == '__main__':
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    print(add(a,b))
