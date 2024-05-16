694074b0b7f0ba5125fa9156c42ba10b849332d2
sbkr74 <sbkr74@gmail.com>
2024-05-16 08:47:35 +0530

    35_Basic.py file updated2.0(applied recursive logic with usr_input)

--- All_programming/35_Basic.py
+++ All_programming/35_Basic.py
@@ -1,1 +1,8 @@
 # Factorial using recursion
+def fact(n):
+    if n<=1:
+        return n
+    else:
+        return fact(n-1)*n
+num = int(input("Enter a number to find factorial: "))
+print("factorial of {} is {}.".format(num,fact(num)))
