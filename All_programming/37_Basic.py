afa3665f4a2d369b0adaa10c29b11230cfed7f95
sbkr74 <sbkr74@gmail.com>
2024-05-20 08:54:03 +0530

    37_Basic.py file added

---
+++ All_programming/37_Basic.py
@@ -0,0 +1,19 @@
+# Adding of two matrices
+A = [[1,2,3],      
+    [4,5,6],
+    [7,8,9]]           
+
+B = [[3,2,1],
+    [6,5,4],
+    [9,8,7]]
+
+result = [[0,0,0],
+          [0,0,0],
+          [0,0,0]]
+
+for i in range(len(A)):
+    for j in range(len(A[0])):
+        result[i][j]=A[i][j]+B[i][j]
+
+for r in result:
+    print(r)
