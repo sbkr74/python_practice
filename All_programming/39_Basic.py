7cf25b49d8c0854bafe915201d0f27e47c03ccee
sbkr74 <sbkr74@gmail.com>
2024-05-20 10:06:32 +0530

    39_Basic.py file updated2.0(with real logic)

--- All_programming/39_Basic.py
+++ All_programming/39_Basic.py
@@ -13,6 +13,7 @@ result = [[0,0,0],
 
 for i in range(len(A)):
     for j in range(len(A[0])):
-        result[i][j]=A[i][j]*B[i][j]
+        for k in range(len(A[0])):
+            result[i][j]+=A[i][k]*B[k][j]
 for r in result:
     print(r)
