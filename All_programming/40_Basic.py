c6c458942c6815e7be3ba61051f4dc037c230ad0
sbkr74 <sbkr74@gmail.com>
2024-05-20 10:25:58 +0530

    40_Basic.py file updated2.0(using functions)

--- All_programming/40_Basic.py
+++ All_programming/40_Basic.py
@@ -1,7 +1,14 @@
 # String palindrome
-
-# using functions
 usr_in = input("Enter a string: ")
+usr_in = usr_in.casefold()
+# using functions
+rev_fun = (reversed(usr_in))
+if list(usr_in) == list(rev_fun):
+    print("palindrome")
+else:
+    print("not palindrome")
+
+# using reversed index
 rev_str = usr_in[::-1]
 if(usr_in == rev_str):
     print(usr_in,'is palindrome as',rev_str)
