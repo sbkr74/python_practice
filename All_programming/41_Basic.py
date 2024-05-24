f4583a07c3f3bd1075a0a662de83a6ce0de00289
sbkr74 <sbkr74@gmail.com>
2024-05-24 22:51:55 +0530

    41_basic.py updated2.0 with new logics

--- All_programming/41_Basic.py
+++ All_programming/41_Basic.py
@@ -1,16 +1,16 @@
 # Remove punctuations from string
 string = "abcd-1234-5678"
-a = list(string)
+string_list = list(string)
 b = []
 c = str()
-for i in range(len(a)):
-    if a[i]=='-':
+for i in range(len(string_list)):
+    if string_list[i]=='-':
         continue
     else:
-        b.append(a[i])
-        c=c+''.join(a[i])
+        b.append(string_list[i])
+        c=c+''.join(string_list[i])
 print(c)
-
+#####################################################
 # Dynamic input 
 a="!@#$%^&*()_+{}:\'\"<>?-=[];,./"
 special_chars=list(a)
@@ -20,3 +20,11 @@ for char in usr_input:
     if char not in special_chars:
         new_string+=''.join(char)
 print(new_string)
+#####################################################
+s=""
+string = '12#$56&*90!@@(87^%43@!'
+new_string_list = list(string)
+for ele in new_string_list:
+    if ele.isalnum():
+        s+=''.join(ele)
+print(s)
