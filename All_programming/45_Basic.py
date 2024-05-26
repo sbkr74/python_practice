04d2d64ca1833e993d6244e4c1c019a53404a431
sbkr74 <sbkr74@gmail.com>
2024-05-26 20:27:38 +0530

    45_Basic.py file updated(removed unused comments and printing statements)

--- All_programming/45_Basic.py
+++ All_programming/45_Basic.py
@@ -1,6 +1,5 @@
 # Merge Mails
 import os
-# current_dir = os.getcwd()
 # Get the absolute path of the script
 script_path = os.path.abspath(__file__)
 
@@ -12,8 +11,6 @@ with open(r"All_programming\names.txt","r",encoding='utf-8') as names_file:
         body = body_file.read()
         for names in names_file:
             mail = "Hi, "+ names + body
-            print(mail)
-            # with open(current_dir+'/'+names.strip()+".txt",'w',encoding='utf-8') as mail_file:
-            # with open("All_programming\\"+names.strip()+".txt",'w',encoding='utf-8') as mail_file:
+            # print(mail)
             with open(script_dir+'\\'+names.strip()+".txt",'w',encoding='utf-8') as mail_file:
-                mail_file.write(mail) 
+                mail_file.write(mail)       # create each file based on names in names.txt
