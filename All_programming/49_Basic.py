927b2a0068b0ee9bbe1eb977c9d61d80fc83dda8
sbkr74 <sbkr74@gmail.com>
2024-06-02 19:51:01 +0530

    49_Basic.py updated2.0

--- All_programming/49_Basic.py
+++ All_programming/49_Basic.py
@@ -23,3 +23,41 @@ obj.dis()
 # print("List: ",obj.add('sam'))
 # print("List: ",obj.add('curran'))
 # print("List: ",obj.delete('sam'))
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+class listOp():
+    def __init__(self):
+        self.n=[]
+    def add(self,ele):
+        return self.n.append(ele)
+    def remove(self,item):
+        return self.n.remove(item)
+    def dis(self):
+        return (self.n)
+
+obj = listOp()
+choice = 1
+while choice != 0:
+    print("0. Exit")
+    print("1. Add")
+    print("2. Remove")
+    print("3. Display")
+
+    choice = int(input("Enter choice: "))
+
+    if choice == 1:
+        n = int(input("Enter number to append: "))
+        obj.add(n)
+
+    elif choice == 2:
+        n = int(input("Enter number to remove: "))
+        obj.remove(n)
+
+    elif choice == 3:
+        print("List: ",obj.dis())
+    
+    elif choice == 0:
+        print("Exiting")
+        break
+    
+    else:
+        print("invalid input choice!")
