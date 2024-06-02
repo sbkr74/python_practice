9f7098bea49e51a81df67d54a2f3fd6654b3316e
sbkr74 <sbkr74@gmail.com>
2024-06-02 16:13:22 +0530

    48_Basic file updated2.0

--- All_programming/48_Basic.py
+++ All_programming/48_Basic.py
@@ -1,14 +1,27 @@
 # area of Rectangle using class
 
-class rectangle:
-    def area(self,l,b):
-        return l*b
+# class rectangle:
+#     def area(self,l,b):
+#         return l*b
     
-    def perimeter(self,l,b):
-        return 2*(l+b)
+#     def perimeter(self,l,b):
+#         return 2*(l+b)
+    
+# l = int(input("Enter length: "))
+# b = int(input("Enter breadth: "))
+# obj = rectangle()
+# print("Area:",obj.area(l,b))
+# print("Perimeter:",obj.perimeter(l,b))
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+class rectangle():
+    def __init__(self,length,width):
+        self.length = length
+        self.width = width
+    def area(self):
+        return self.length*self.width
     
 l = int(input("Enter length: "))
-b = int(input("Enter breadth: "))
-obj = rectangle()
-print("Area:",obj.area(l,b))
-print("Perimeter:",obj.perimeter(l,b))
+w = int(input("Enter width: "))
+obj = rectangle(l,w)
+print("Area: ",obj.area())
