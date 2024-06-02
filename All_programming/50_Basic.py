d4bdbb5d343bc23bdc9b43ee49244d223e76d005
sbkr74 <sbkr74@gmail.com>
2024-06-02 20:52:55 +0530

    50_Basic.py updated4.0

--- All_programming/50_Basic.py
+++ All_programming/50_Basic.py
@@ -14,7 +14,47 @@ print("Addition:",obj.add(7,3))
 print("Substraction:",obj.sub(7,3))
 print("Multiplication:",obj.mult(7,3))
 print("Division:",obj.div(7,3))
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+class cal():
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
 
+    def add(self):
+        return self.a + self.b
+    def sub(self):
+        return self.a - self.b
+    def div(self):
+        return self.a//self.b
+    def mult(self):
+        return self.a*self.b
+
+a = int(input("Enter a first number: "))
+b = int(input("Enter a second number: "))
+
+obj = cal(a,b)
+choice = 1
+while choice != 0:
+    print("0. Exit")
+    print("1. addition")
+    print("2. substration")
+    print("3. multiplication")
+    print("4. division")
+
+    choice = int(input("Enter choice: "))
+    if choice == 1 :
+        print("Result:",obj.add())
+    elif choice == 2:
+        print("Result:",obj.sub())
+    elif choice == 3:
+        print("Result:",obj.mult())
+    elif choice == 4:
+        print("Result:",obj.div())
+    elif choice == 0:
+        print("Exiting...")
+    else:
+        print("Wrong operation!")
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 class calculator():
     def __init__(self):
         self.n = []
@@ -69,5 +109,3 @@ while choice != 0:
         break
     else:
         print("Wrong operation!")
-
-
