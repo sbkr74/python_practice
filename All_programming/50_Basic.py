54fc8b5ae684c878371ef8da5213c8522bd4de8c
sbkr74 <sbkr74@gmail.com>
2024-06-02 20:12:55 +0530

    50_Basic.py updated2.0

--- All_programming/50_Basic.py
+++ All_programming/50_Basic.py
@@ -14,3 +14,42 @@ print("Addition:",obj.add(7,3))
 print("Substraction:",obj.sub(7,3))
 print("Multiplication:",obj.mult(7,3))
 print("Division:",obj.div(7,3))
+
+class calculator():
+    def __init__(self):
+        self.n = []
+    def add(self,a):
+        self.n.append(a)
+        return sum(self.n)
+    def sub(self,a):
+        self.n.append(a)
+        return sum(self.n)
+    def result(self):
+        return sum(self.n)
+
+obi = calculator()
+choice = 1
+while choice != 0:
+    print("0. Exit")
+    print("1. addition")
+    print("2. substration")
+    print("3. result")
+
+    choice = int(input("Enter choice: "))
+
+    if choice == 1:
+        obi.add(int(input("enter positive number: ")))
+
+    elif choice == 2:
+        obi.sub(int(input("enter negative number: ")))
+    
+    elif choice == 3:
+        print(obi.result())
+    
+    elif choice == 0:
+        print("exiting")
+        break
+    else:
+        print("Wrong operation!")
+
+
