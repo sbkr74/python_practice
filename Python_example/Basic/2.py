058857d891900af0993868dea9715e8f0a9eafa8
sbkr74 <sbkr74@gmail.com>
2024-06-03 11:15:43 +0530

    2.py file added

---
+++ Python_example/Basic/2.py
@@ -0,0 +1,19 @@
+# static operation
+a = 25.0
+b = 10
+c = a+b
+d = a-b
+e = a*b
+f = a/b
+g = "Hello"
+h = 'P'
+
+# printing Statement
+print("A:",a)
+print("B:",b)
+print("Sum:",c)
+print("Sub:",d)
+print("Mult:",e)
+print("Div:",f)
+print("String:",g)
+print("Char:",h)
