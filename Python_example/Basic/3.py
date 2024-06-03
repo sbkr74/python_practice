0201d029d42770d63b485502a49987b494af9c28
sbkr74 <sbkr74@gmail.com>
2024-06-03 11:23:09 +0530

    3.py file added

---
+++ Python_example/Basic/3.py
@@ -0,0 +1,19 @@
+# User Scan Example
+s = input("Enter a String: ")
+i = int(input("Enter a decimal value: "))
+f = float(input("Enter a float value: "))
+
+add = i+f
+sub = i-f
+mult = i*f
+fdiv = i/f
+idiv = int(i//f)
+
+print("String:",s)
+print("Integer:",i)
+print("Float:",f)
+print("Add:",add)
+print("Sub:",sub)
+print("Mult:",mult)
+print("Float div:",fdiv)
+print("Int div:",idiv)
