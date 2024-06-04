5158c28e1fa61efca4aedaec54cac6ace943870d
sbkr74 <sbkr74@gmail.com>
2024-06-04 22:07:58 +0530

    10.py file added

---
+++ Python_example/Basic/10.py
@@ -0,0 +1,7 @@
+# Simple Interest Calculation Example
+# si = p*r*t/100
+p = int(input("Enter principal amount: "))
+r = int(input("Enter Interest rate: "))
+t = int(input("Enter time period: "))
+si = (p*r*t)/100
+print("Simple Interest:",si)
