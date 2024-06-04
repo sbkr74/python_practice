5d7acd8285046c94c26a7434bc786fe0b774fa82
sbkr74 <sbkr74@gmail.com>
2024-06-04 21:54:22 +0530

    8.py file added

---
+++ Python_example/Basic/8.py
@@ -0,0 +1,6 @@
+# Value of Circle (Import Math Library)
+import math as m
+r = float(input("Enter radius: "))
+area = m.pi*r*r
+print("Area of Circle:",round(area,2))
+print("Area: {:.2f}".format(area))
