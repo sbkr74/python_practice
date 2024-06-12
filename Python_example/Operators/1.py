6dc4c54a741701bed667e17cbca50f4f73b1f085
sbkr74 <sbkr74@gmail.com>
2024-06-12 21:41:22 +0530

    1.py file added

---
+++ Python_example/Operators/1.py
@@ -0,0 +1,10 @@
+# Arithmetical Operator
+x = 5
+y = 3
+print("x+y:",(x+y))
+print("x-y:",(x-y))
+print("x*y:",(x*y))
+print("x%y:",(x%y))
+print("x//y:",(x//y))
+print("x/y:",(x/y))
+print("x**y:",(x**y))
