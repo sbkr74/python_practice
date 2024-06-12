84ad240716cd1c11c2c98875707164fa829613b5
sbkr74 <sbkr74@gmail.com>
2024-06-12 22:02:49 +0530

    3.py file added

---
+++ Python_example/Operators/3.py
@@ -0,0 +1,15 @@
+# Bitwise Operators
+a = 3
+b = 2 
+c = 16
+
+print("bin>>",bin(a))
+print("octal>>",oct(a))
+print("hexadecimal>>",hex(c))
+print("decimal>>",int(0b10))
+print(a>>3)
+print(a<<3)
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(~a)
