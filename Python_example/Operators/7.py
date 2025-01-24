02483e90b294723f310aee1fe209b4f05064abce
sbkr74 <sbkr74@gmail.com>
2025-01-24 20:21:05 +0530

    7.py file added

---
+++ Python_example/Operators/7.py
@@ -0,0 +1,21 @@
+# 7. Membership Operator
+
+a = 10
+b = 18
+
+myList = [3,6,9,12,15,18]
+
+if a in myList:
+    print("answer1: True")
+else:
+    print("answer1: False")
+
+if a not in myList:
+    print("answer2: True")
+else:
+    print("answer2: False")
+
+if b in myList:
+    print("answer3: True")
+else:
+    print("answer3: False")
