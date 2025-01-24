b61b86e0f4398089a4572b57e2e1cceefae5966e
sbkr74 <sbkr74@gmail.com>
2025-01-24 20:07:11 +0530

    5.py file added

---
+++ Python_example/Operators/5.py
@@ -0,0 +1,19 @@
+# 5. Assignment Operator
+
+a = 10
+b = 15
+c = a+b
+print(c)                    # 15
+
+# shortend operation
+c+=a
+print(c)                    # 35
+
+c-=b
+print(c)                    # 20
+
+c*=a
+print(c)                    # 200
+
+c/=a
+print(c)                    # 20.0
