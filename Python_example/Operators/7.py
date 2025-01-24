42fee33d912170044c1cb5163773d5c341b5a1ae
sbkr74 <sbkr74@gmail.com>
2025-01-24 20:26:31 +0530

    update1.0[another example]

--- Python_example/Operators/7.py
+++ Python_example/Operators/7.py
@@ -19,3 +19,13 @@ if b in myList:
     print("answer3: True")
 else:
     print("answer3: False")
+
+
+# Another Example
+x = "Hello World"
+y = {1:'a',2:'b'}
+
+print("H in x:",('H' in x))                     # True
+print("hello not in x:",('hello' not in x))     # True
+print("1 in y:",(1 in y))                       # True
+print("a in y:",('a' in y))                     # False
