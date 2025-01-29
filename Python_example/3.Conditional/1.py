fefc057ff680b529c11003d91e0e1ee3315432af
sbkr74 <sbkr74@gmail.com>
2025-01-29 22:02:50 +0530

    1.py file added

---
+++ Python_example/3.Conditional/1.py
@@ -0,0 +1,8 @@
+# Simple if
+marks = int(input("Enter your Marks: "))
+if marks >100 or marks<0:
+    print("Out of Subjected Area..")
+if marks>=35 and marks<=100:
+    print("Horray!!! You have passed.")
+if marks<35 and marks>=0:
+    print("You have Failed. Try Harder Next Time.")
