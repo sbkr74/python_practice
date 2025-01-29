923821ce75a31d79af105c88a23ba02f5a16588e
sbkr74 <sbkr74@gmail.com>
2025-01-29 22:49:16 +0530

    original script

--- Python_example/3.Conditional/5.py
+++ Python_example/3.Conditional/5.py
@@ -1,12 +1,17 @@
 # Check leap year
 year = int(input("Enter year: "))
 
-# if year%400==0:
-#     print(year,"is Leap Year.")
-# if year%100==0:
-#     print(year,"is not Leap year.")
-
 if year%4==0:
     print(year,"is Leap year")
 else:
     print(year,"is not Leap year.")
+
+
+# Original way
+if year%100==0:
+    if year%400==0 and year%4==0:
+        print(year,"is Leap year.")
+    else:
+        print(year,"is not Leap Year.")
+else:
+    print(year,"is not leap year")
