b81231c89a5395167c965ad0a52d110307063b54
sbkr74 <sbkr74@gmail.com>
2025-03-03 20:09:27 +0530

    8.py file added

---
+++ Python_example/3.Conditional/8.py
@@ -0,0 +1,13 @@
+# find maximum between 2 values
+num1 = int(input("Enter your 1st number: "))
+num2 = int(input("Enter your 2nd number: "))
+
+if num1>num2:
+    print(f"1st number:{num1} is greater than 2nd number:{num2}.")
+elif num1<num2:
+    print(f"2nd number:{num2} is greater than 1st number:{num1}.")
+else:
+    print(f"both are equal. {num1} == {num2}")
+
+# maximum between 3 numbers
+# upcoming for upto n numbers
