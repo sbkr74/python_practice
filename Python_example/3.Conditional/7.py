468f079026853559e380675c3b0fa2289c6c9d36
sbkr74 <sbkr74@gmail.com>
2025-03-03 20:01:47 +0530

    7.py file added

---
+++ Python_example/3.Conditional/7.py
@@ -0,0 +1,9 @@
+# check integer is +ve,-ve or 0.
+print("check integer is +ve,-ve or 0.")
+num = int(input("Enter a number: "))
+if num>0:
+    print(num,"is positive.")
+elif num<0:
+    print(num,"is negative")
+else:
+    print(num,"is zero.")
