376d84f2dc37c29f1aa2b47af55650a3942843c2
sbkr74 <sbkr74@gmail.com>
2025-03-06 22:22:35 +0530

    update1.0[err]

--- Python_example/3.Conditional/12.py
+++ Python_example/3.Conditional/12.py
@@ -5,6 +5,7 @@ yrs_of_exp = int(input("Years Of Exp: "))
 sal = int(input("Salary: "))
 dept = int(input("1.Tech\n2.Management\nChoose your Dept: "))
 hike = 0
+err = "Something went wrong.\nSelected Wrong option"
 if dept == 1:
     sub_dept = int(input("1.AI\ML\n2.Data Engg\n3.IT\nChoose your Dept: "))
     if sub_dept == 1:
@@ -17,7 +18,7 @@ if dept == 1:
         inc = (sal*15)/100
         hike = sal+inc
     else:
-        print("Something went wrong.\nSelected Wrong option")
+        print(err)
     print(f"EmpNo:{emp_id}\tupdated Sal:${hike}")
 elif dept == 2:
     sub_dept = int(input("1.Managers\n2.Sales\n3.Promoters\4.HR\nChoose your Dept: "))
@@ -34,7 +35,7 @@ elif dept == 2:
         inc = (sal*10)/100
         hike = sal+inc
     else:
-        print("Something went wrong.\nSelected Wrong option")
+        print(err)
     print(f"{emp_id}:${hike}")
 else:
-    print("Something went wrong.\nSelected Wrong option")
+    print(err)
